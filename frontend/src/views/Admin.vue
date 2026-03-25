7970f978e01308853e01886ffadc396c07866fe8
apknation <atanasikafuka@gmail.com>
2026-03-25 20:32:36 +0300

    Fix missing end tag in products tab section

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -745,7 +745,9 @@
             </div>
           </div>
         </div>
-      
+      </div>
+    </div>
+    
     <!-- Order Details Modal -->
     <div v-if="selectedOrder" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div class="bg-white rounded-xl p-6 max-w-2xl w-full mx-4 max-h-[90vh] overflow-y-auto">
